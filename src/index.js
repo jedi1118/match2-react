b7bb3f31e08d2de7ab7ce2e6c8cdb500afcf0275
Shane Lin <shalin@ebay.com>
2022-03-28 13:45:22 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+console.log('#####');
+
+ReactDOM.render(<h1>hello</h1>, document.getElementById('root'));
